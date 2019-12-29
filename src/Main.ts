0d57169ad549262bb5d19ad4f5ac2746647848c0
Kevin Villanueva <undyingmind@gmail.com>
2019-12-29 13:21:31 +0800

    Added default container intance

--- src/Main.ts
+++ src/Main.ts
@@ -1,8 +1,12 @@
+import Container from "./Container";
+export { Container };
+
 export { default as ICollection } from "./interfaces/ICollection";
 export { default as IContainer } from "./interfaces/IContainer";
 export { default as IProvider } from "./interfaces/IProvider";
 
 export { default as Collection } from "./Collection";
-export { default as Container } from "./Container";
 export { default as Provider } from "./Provider";
 export { default as Service } from "./Service";
+
+export default new Container();
